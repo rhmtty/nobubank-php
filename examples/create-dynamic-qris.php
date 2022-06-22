9a435c22e1be125b6e753e34105004b40a4a27a7
zerosdev <zerosdev@users.noreply.github.com>
2022-06-22 14:55:39 +0000

    phpcs: apply coding style

--- examples/create-dynamic-qris.php
+++ examples/create-dynamic-qris.php
@@ -19,42 +19,42 @@ $nobu = new Client($mode, $config);
 $qris = $nobu->qris()
     /**
      * Set transaction number
-     * 
+     *
      * @param string $transactionNo
      */
     ->setTransactionNo('ABCDEFGHIJKLMNO')
 
     /**
      * Set reference number
-     * 
+     *
      * @param string $referenceNo
      */
     ->setReferenceNo('1234567890')
 
     /**
      * Set payment amount
-     * 
+     *
      * @param int $amount
      */
     ->setAmount(1000)
 
     /**
      * Set QR validity time in seconds
-     * 
+     *
      * @param int $validTime
      */
     ->setValidTime(3600)
 
     /**
      * Set displayed store name
-     * 
+     *
      * @param string $storeName
      */
     ->setStoreName('Zeros Technology')
 
     /**
      * Set displayed city name
-     * 
+     *
      * @param string $cityName
      */
     ->setCityName('Ponorogo')
