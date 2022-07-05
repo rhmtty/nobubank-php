75bf4e878760c0a0ba132a17bc81642d1fcde3ef
Zeros Developer <33526722+zerosdev@users.noreply.github.com>
2022-07-05 11:20:01 +0700

    Update URL

--- src/Constant.php
+++ src/Constant.php
@@ -5,11 +5,13 @@ namespace ZerosDev\NobuBank;
 class Constant
 {
     public const URL_API_DEVELOPMENT = 'http://uatmerchant.nobubank.com:2104/';
-    public const URL_API_PRODUCTION = 'http://uatmerchant.nobubank.com:2104/';
+    public const URL_API_PRODUCTION = 'https://merchant.nobubank.com/';
+    
     public const URL_STATUS_DEVELOPMENT = 'http://uatmerchantnotif.nobubank.com/';
-    public const URL_STATUS_PRODUCTION = 'http://uatmerchantnotif.nobubank.com/';
+    public const URL_STATUS_PRODUCTION = 'https://merchantnotif.nobubank.com/';
+    
     public const URL_CANCEL_DEVELOPMENT = 'http://uatmerchant.nobubank.com:2101/';
-    public const URL_CANCEL_PRODUCTION = 'http://uatmerchant.nobubank.com:2101/';
+    public const URL_CANCEL_PRODUCTION = 'https://merchant.nobubank.com/';
 
     public const MERGE = "merge";
 
