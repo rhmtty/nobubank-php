b265686fb50dd7afa770a9ea9c4e5f9d81c1f8e0
zerosdev <zerosdev@users.noreply.github.com>
2022-07-05 04:20:15 +0000

    phpcs: apply coding style

--- src/Constant.php
+++ src/Constant.php
@@ -6,10 +6,10 @@ class Constant
 {
     public const URL_API_DEVELOPMENT = 'http://uatmerchant.nobubank.com:2104/';
     public const URL_API_PRODUCTION = 'https://merchant.nobubank.com/';
-    
+
     public const URL_STATUS_DEVELOPMENT = 'http://uatmerchantnotif.nobubank.com/';
     public const URL_STATUS_PRODUCTION = 'https://merchantnotif.nobubank.com/';
-    
+
     public const URL_CANCEL_DEVELOPMENT = 'http://uatmerchant.nobubank.com:2101/';
     public const URL_CANCEL_PRODUCTION = 'https://merchant.nobubank.com/';
 
